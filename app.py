c26a35f2bce9c96f47a105a94fd3add4f385eabe
Thomas Ward <tomward0606@icloud.com>
2025-07-17 18:44:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_key')
